c9f80112ddd851f7533c39298b02bf512144d73b
Beryl01 <berylnegesa@gmail.com>
2020-02-21 08:17:57 +0300

    my jquery functions

--- index.js
+++ index.js
@@ -2,95 +2,77 @@
 //toggle
 $(document).ready(function(){
   $("#box").click(function(){
-    $("#design-img").slideToggle(1000);
-    $("#design").slideToggle(1200);
+    $("#design-img").slideToggle();
+    $("#design").slideToggle();
   });
-});
 
-$(document).ready(function(){
   $("#box").click(function(){
-    $("#development-img").slideToggle(1000);
-    $("#development").slideToggle(1200);
+    $("#development-img").slideToggle();
+    $("#development").slideToggle();
   });
-});
 
-$(document).ready(function(){
   $("#box").click(function(){
-    $("#product-img").slideToggle(1000);
-    $("#product").slideToggle(1200);
+    $("#product-img").slideToggle();
+    $("#product").slideToggle();
   });
 });
   
 //portfio overlay 
 $(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work1").show(1000);
-    $("#overlay1").hide(1200);
+    $("#work1").show();
+    $("#overlay1").hide();
   });
-});
 
-$(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work2").show(1000);
-    $("#overlay2").hide(1200);
+    $("#work2").show();
+    $("#overlay2").hide();
   });
-});
 
-$(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work3").show(1000);
-    $("#overlay3").hide(1200);
+    $("#work3").show();
+    $("#overlay3").hide();
   });
-});
 
-$(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work4").show(1000);
-    $("#overlay4").hide(1200);
+    $("#work4").show();
+    $("#overlay4").hide();
   });
-});
 
-$(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work5").show(1000);
-    $("#overlay6").hide(1200);
+    $("#work5").show();
+    $("#overlay6").hide();
   });
-});
 
-$(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work7").show(1000);
-    $("#overlay7").hide(1200);
+    $("#work7").show();
+    $("#overlay7").hide();
   });
-});
 
-$(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work8").show(1000);
-    $("#overlay9").hide(1200);
+    $("#work8").show();
+    $("#overlay9").hide();
   });
-});
 
-$(document).ready(function(){
   $(".row").mouseover(function(){
-    $("#work1").show(1000);
-    $("#overlay1").hide(1200);
+    $("#work1").show();
+    $("#overlay1").hide();
   });
 });
 
-  //form function
-  $(document).ready(function(){
-    $("form#form1").submit(function(event){
-      var name = $("input#name").value();
-      var email = $("input#email").value();
-      var message = $("textarea#comment").value();
-      if ($("input#name").val() && $("input#email").value()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
+  // //form function
+  // $(document).ready(function(){
+  //   $("form#form1").submit(function(event){
+  //     var name = $("input#name").value();
+  //     var email = $("input#email").value();
+  //     var message = $("textarea#comment").value();
+  //     if ($("input#name").val() && $("input#email").value()){
+  //       alert (name + ", we have received your message. Thank you for reaching out to us.");
+  //     }
+  //     else {
+  //       alert("Please enter your name and email!");
+  //     }
       
-    });
+  //   });
   
-  });
+  // });
