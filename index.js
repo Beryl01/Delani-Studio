978e533522fc3c6efb75ce609808a77a2d3c1541
Beryl01 <berylnegesa@gmail.com>
2020-02-21 12:21:15 +0300

    jquery correction on toggle

--- index.js
+++ index.js
@@ -2,77 +2,76 @@
 //toggle
 $(document).ready(function(){
   $("#box").click(function(){
-    $("#design-img").slideToggle();
-    $("#design").slideToggle();
+    $("#design-img").toggle();
+    $("#design").toggle();
   });
 
   $("#box").click(function(){
-    $("#development-img").slideToggle();
-    $("#development").slideToggle();
+    $("#development-img").toggle();
+    $("#development").toggle();
   });
 
   $("#box").click(function(){
-    $("#product-img").slideToggle();
-    $("#product").slideToggle();
+    $("#product-img").toggle();
+    $("#product").toggle();
   });
 });
   
 //portfio overlay 
 $(document).ready(function(){
-  $(".row").mouseover(function(){
+  $(".row").hover(function(){
     $("#work1").show();
     $("#overlay1").hide();
   });
 
-  $(".row").mouseover(function(){
+  $(".row").hover(function(){
     $("#work2").show();
     $("#overlay2").hide();
   });
 
-  $(".row").mouseover(function(){
+  $(".row").hover(function(){
     $("#work3").show();
     $("#overlay3").hide();
   });
 
-  $(".row").mouseover(function(){
+  $(".row").hover(function(){
     $("#work4").show();
     $("#overlay4").hide();
   });
 
-  $(".row").mouseover(function(){
+  $(".row").hover(function(){
     $("#work5").show();
+    $("#overlay5").hide();
+  });
+
+  $(".row").hover(function(){
+    $("#work6").show();
     $("#overlay6").hide();
   });
 
-  $(".row").mouseover(function(){
+  $(".row").hover(function(){
     $("#work7").show();
     $("#overlay7").hide();
   });
 
-  $(".row").mouseover(function(){
+  $(".row").hover(function(){
     $("#work8").show();
-    $("#overlay9").hide();
-  });
-
-  $(".row").mouseover(function(){
-    $("#work1").show();
-    $("#overlay1").hide();
+    $("#overlay8").hide();
   });
 });
 
   // //form function
-  // $(document).ready(function(){
-  //   $("form#form1").submit(function(event){
-  //     var name = $("input#name").value();
-  //     var email = $("input#email").value();
-  //     var message = $("textarea#comment").value();
-  //     if ($("input#name").val() && $("input#email").value()){
-  //       alert (name + ", we have received your message. Thank you for reaching out to us.");
-  //     }
-  //     else {
-  //       alert("Please enter your name and email!");
-  //     }
+  $(document).ready(function(){
+    $("form#form1").submit(function(){
+      var name = $("input#name").value();
+      var email = $("input#email").value();
+      var message = $("textarea#comment").value();
+      if ($("input#name").value() && $("input#email").value()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
       
-  //   });
-  
-  // });
+    });
+  });
